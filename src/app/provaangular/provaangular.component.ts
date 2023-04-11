1f9f699e64c4c9d8b318ea78cbc06141d5621b26
nickvalle <andreapps.com@gmail.com>
2023-04-11 08:40:00 -0300

    provinha versao 2

--- src/app/provaangular/provaangular.component.ts
+++ src/app/provaangular/provaangular.component.ts
@@ -9,9 +9,11 @@ export class ProvaangularComponent {
   reais : number = 0;
   dolar : number = 0;
   converte : number = 0;
+  converte2 : number = 0;
+  converte3 : number = 0;
 
   converter(){
-     this.converte 
+     this.converte
   }
 
 }
